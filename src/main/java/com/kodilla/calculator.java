837dc4ac99429c58217a911fda7d8463096b283d
Maciej Szalaj <szalajm.neo@gmail.com>
2019-04-07 10:53:30 +0200

    Calculator Commit

---
+++ src/main/java/com/kodilla/calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla;
+
+public class calculator {
+
+    public int addAToB (int a, int b) {
+        return a + b;
+    }
+
+    public int subtractAFromB(int a, int b) {
+        return a - b;
+    }
+}
